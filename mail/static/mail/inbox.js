b0cd1be2393f048865f816d91f34631f8aab52dc
Margarita Sirbu <margaritapetru@hotmail.com>
2021-09-22 00:02:13 -0700

    Add the functionality to send an email and load it to the appropriate mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('form').onsubmit = submit;
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -29,5 +30,52 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  const emails_view = document.querySelector('#emails-view');
+  emails_view.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  //Show the sent emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+    emails.forEach(function(email) {
+      let emailHTML = '';
+      
+      emailHTML += `<div class="emailStyle">`;
+      emailHTML += `<div class="date">${email.timestamp}</div>`;
+      emailHTML += `<div class="email_address">${email.recipients.join(', ')}</div>`;
+      emailHTML += `<div class="subject">${email.subject}</div>`;
+      emailHTML += `<div class="body">${email.body.substring(0, 50)}</div>`;
+      emailHTML += `</div>`;
+      emails_view.innerHTML += emailHTML;
+      console.log(emails_view.innerHTML);
+    })
+  })
+  .catch(error => {
+   console.log('Error:', error);
+  });
+}
+
+function submit() {
+
+  //make POST request to /emails and create a JSON object values for recipients, subject and body
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  })  
+  .then(response => response.json())
+  .then(result => {
+    load_mailbox('sent'); 
+  })
+  // Catch any errors and log them to the console
+  .catch(error => {
+    console.log('Error:', error);
+  });
+  // Prevent default submission
+  return false;
 }
